595c33ec6090fea160a7a99b7a0e8f2f27a40c45
Serg <sergfmoron@outlook.com>
2024-11-14 08:21:45 -0400

    l

---
+++ AppWebRazor/Pages/Dropshipping/ProsAndCons.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AppWebRazor.Pages.Dropshipping
+{
+    public class ProsAndConsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
